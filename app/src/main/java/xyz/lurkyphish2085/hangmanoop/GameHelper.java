a40591e70e472fc897bb51baa7adb7a71eaf1125
dntrcmp420 <dntrcmp420@damai-e526>
2022-10-03 16:56:47 +0800

    clearScreen() implemented in GameHelper class

--- app/src/main/java/xyz/lurkyphish2085/hangmanoop/GameHelper.java
+++ app/src/main/java/xyz/lurkyphish2085/hangmanoop/GameHelper.java
@@ -1,12 +1,33 @@
 package xyz.lurkyphish2085.hangmanoop;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class GameHelper {
 
 	private static final Scanner SCAN = new Scanner(System.in);
+	private static final ProcessBuilder PROCESS = new ProcessBuilder();
+	private static final String OS = System.getProperty("os.name");
+
 
 	public static String getUserInput() {
 			return SCAN.nextLine();
+	}
+
+	public static void clearScreen() {
+		try {
+			if (OS.equals("Windows")) {
+				PROCESS.command("cmd", "/c", "cls");
+				PROCESS.inheritIO().start().waitFor();
+				return;
+			}
+
+			PROCESS.command("clear");
+			PROCESS.inheritIO().start().waitFor();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
 	}
 }
