9e92b16784d6a786893538a5b4fd239f0a05dc6b
dntrcmp420 <dntrcmp420@damai-e526>
2022-10-22 19:38:50 +0800

    GameHelper is now final and cannot be instantiated

--- app/src/main/java/xyz/lurkyphish2085/hangmanoop/GameHelper.java
+++ app/src/main/java/xyz/lurkyphish2085/hangmanoop/GameHelper.java
@@ -3,12 +3,13 @@ package xyz.lurkyphish2085.hangmanoop;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class GameHelper {
+public final class GameHelper {
 
 	private static final Scanner SCAN = new Scanner(System.in);
 	private static final ProcessBuilder PROCESS = new ProcessBuilder();
 	private static final String OS = System.getProperty("os.name");
 
+	private GameHelper() {}
 
 	public static String getUserInput() {
 			return SCAN.nextLine();
