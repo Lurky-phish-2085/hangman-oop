d7b16a154839eb0d0e4cab3dd8aeec4e366e3220
lurkyphish2085 <Lurky-phish-2085@users.noreply.github.com>
2022-11-04 17:24:14 +0800

    ErrorExit.java added

---
+++ app/src/main/java/xyz/lurkyphish2085/hangmanoop/ErrorExit.java
@@ -0,0 +1,27 @@
+package xyz.lurkyphish2085.hangmanoop;
+public class ErrorExit {
+
+	public ErrorExit(Exception ex) {
+		printMessage(ex.getMessage());
+		System.exit(1);
+	}
+
+	public ErrorExit(String message) {
+		printMessage(message);
+		System.exit(1);
+	}
+
+	public ErrorExit(String message, int exitCode) {
+		if (exitCode < 0) {
+			throw new IllegalArgumentException("exit code should never be negative");
+		}
+
+		printMessage(message);
+		System.exit(exitCode);
+	}
+
+	private void printMessage(String message) {
+		System.err.println("hangman: " + message);
+		System.err.println("Try 'hangman' -h' for more information");
+	}
+}
